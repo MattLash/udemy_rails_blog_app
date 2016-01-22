ec6a1640b7e92fbc58988746160526cdf58049f8
Matt Lashinsky <malashinsky@gmail.com>
2016-01-22 16:25:31 +0000

    articles pt duex

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,25 @@ class ArticlesController < ApplicationController
     end
     
     def create
-       #render plain: params[:article].inspect
-       @article = Article.new(article_params)
-       @article.save
-       redirect_to articles_show(@article)
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        @article.save
+        if @article.save
+            flash[:notice] = "You made the article, Woo!"
+            redirect_to article_path(@article)
+        else
+            flash[:notice] = "you done fucked up!"
+            render 'new'
+        end
+    end
+    
+    def show
+       @article = Article.find(params[:id]) 
     end
     
     private
         def article_params
-           params.require(:article).permit(:title, :description) 
+            params.require(:article).permit(:title, :description) 
             
         end
   
