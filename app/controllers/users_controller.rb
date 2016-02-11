03e45cc16dc5f2972a72d9f98a744568527cf50e
Matt Lashinsky <malashinsky@gmail.com>
2016-02-11 20:37:43 +0000

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,12 @@
 class UsersController < ApplicationController
     
     def index
-       @users = User.all 
+       @users = User.paginate(page: params[:page], per_page: 5) 
     end
     
     def new
         @user = User.new 
     end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     def create
         @user = User.new(user_params)
         if @user.save
