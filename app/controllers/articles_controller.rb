42845ec5b017969ccadecc3b1e2b6d619b4c53cd
Matt Lashinsky <malashinsky@gmail.com>
2016-02-11 14:45:57 +0000

    added meta deta to listing details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     end
     def create
         #render plain: params[:article].inspect
-        debugger
+        # debugger
         @article = Article.new(article_params)
         @article.user = User.first
         if @article.save
