f86d40d7677b3b2fd49a75998db85ea32c7fe18d
Matt Lashinsky <malashinsky@gmail.com>
2016-02-16 22:15:52 +0000

    updated listcategorytest name

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
 
     def setup
         @category = Category.create(name: "cars")
@@ -12,8 +12,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_template 'categories/index'
         assert_select "a[href=?]", category_path(@category), text: @category.name 
         assert_select "a[href=?]", category_path(@category2), text: @category2.name 
-        
-        
+         
     end
     
 end
